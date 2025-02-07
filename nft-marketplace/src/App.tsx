a0646f2ca2abc76c83f38a4a38e63618b6fed3ee
Raphaël P. <raphaelpavard@gmail.com>
2025-02-07 12:56:05 +0100

    feat: wallet auth with meta mask

--- nft-marketplace/src/App.tsx
+++ nft-marketplace/src/App.tsx
@@ -1,13 +1,17 @@
-import NftList from "./components/NtfList/NftList"
+import NftList from "./components/NtfList/NftList";
+import { useState } from "react";
+import WalletConnector from "./components/WalletConnector/WalletConnector";
 
 function App() {
+  const [account, setAccount] = useState<string | null>(null);
+
   return (
     <>
       <h1>NTF APP</h1>
-
+      <WalletConnector onAccountChange={setAccount} />
       <NftList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
